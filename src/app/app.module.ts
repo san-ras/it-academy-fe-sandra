3f8281979e8657442003f3ca2155f5e2df379557
san-ras <sandra.raslane@gmail.com>
2022-04-06 19:23:01 +0300

    s

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,21 +3,19 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import {HttpClientModule} from "@angular/common/http";
+import { HttpClientModule } from '@angular/common/http';
 import { HerokuTestComponent } from './heroku-test/heroku-test.component';
 
-
 @NgModule({
-  declarations: [
-    AppComponent,
-    HerokuTestComponent
-  ],
+  declarations: [AppComponent, HerokuTestComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    HerokuTestComponent,
   ],
-  providers: [HttpClientModule],
-  bootstrap: [AppComponent]
+  providers: [],
+  bootstrap: [AppComponent],
+  exports: [HerokuTestComponent],
 })
-export class AppModule { }
+export class AppModule {}
